8d09d8214764e8fae24c1c1abe9d33c4f8e42026
easyball57 <easyball57@users.noreply.github.com>
2019-01-10 11:53:24 +0100

    Add pie and reset

--- simplesurveyapp.py
+++ simplesurveyapp.py
@@ -67,3 +67,65 @@ def list_survey():
 	for row in rows:
 		result = result + str(row[0]) + "<br>"
 	return result
+
+@app.route('/reset/<int:survey_id>')
+def reset_survey():
+	# add the answer NO to the survey id survey_id
+	con = lite.connect(dbFile)
+	con.execute("DELETE * FROM SSA WHERE survey_id=?", (survey_id,))
+	con.commit()
+	con.close()
+	result = 'Survey #' + str(survey_id) + ' has been deleted.'
+	return result
+
+@app.route('/pie/<int:survey_id>')
+def pie_survey():
+	# show the list of answers for a given survey id survey_id
+	con = lite.connect(dbFile)
+	cur = con.cursor()
+	cur.execute("SELECT survey_id, answer, count(*) FROM SSA WHERE survey_id=? GROUP BY answer", (survey_id,))
+	rows = cur.fetchall()
+	con.close()
+	result = '''
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
+
+<div id="piechart"></div>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+// Load google charts
+google.charts.load("current", {"packages":["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+	'''
+	result = result + 'var survey_nb=' + str(survey_id) + ';'
+	result = result + 'var yes_no=' + str(rows[0][0]) + ';'
+	result = result + 'var yes_nb=' + str(rows[1][0]) + ';'
+	result = '''
+  var data = google.visualization.arrayToDataTable([
+  ["Answers", "Numbers"],
+  ["Yes", yes_nb],
+  ["No", no_nb]
+]);
+  // Optional; add a title and set the width and height of the chart
+  var options = {"title":"Survey #"+survey_nb, "width":550, "height":400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById("piechart"));
+  chart.draw(data, options);
+}
+</script>
+
+</body>
+</html>
+  	'''
+	return result
+	
+	
+
+	
